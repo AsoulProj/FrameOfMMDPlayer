d60adf4fe6cd346945013bb40e017714e1e19eaa
ChenZihan <64928288+ChenZihan-sudo@users.noreply.github.com>
2022-04-05 23:37:54 +0800

    Upload by hitora

--- MMDPlayManager.js
+++ MMDPlayManager.js
@@ -1,16 +1,42 @@
 let MMDPlayManager = class {
 
+    maxPlayEnd = -1;
+    minPlayEnd = -1;
 
-    constructor(params = {}, ClassOfDurationDatas) {
+    constructor(params = {}, ClassOfDurationData) {
         this.configuration = {
             loopPlay: params.loopPlay || false,
-        }
-        if (ClassOfDurationDatas != undefined) {
+            playEnd: params.playEnd || "minPlayEnd",
 
+        }
+        if (ClassOfDurationData != undefined) {
+            if (durationDatas.constructor.name == 'DurationData') {
+                this.totalDuration = ClassOfDurationData.totalDuration;
+                for (const key in totalDuration) {
+                    if (totalDuration[key] > 0) {
+                        if (totalDuration[key] > maxPlayEnd)
+                            this.maxPlayEnd = totalDuration[key]
+                        if (totalDuration[key] < minPlayEnd || minPlayEnd < 0)
+                            this.minPlayEnd = totalDuration[key]
+                    }
+                }
+            } else throw new Error("MMDPlayManager: Unvaild instance. Use DurationData instance");
         }
     }
 
     rePlay() {
 
+    }
+
+    //检查中断暂停的位置，之后再决定是否继续播放
+    checkPlayEnd(duration) {
+        if (maxPlayEnd < 0 && minPlayEnd < 0) {
+
+        }
+
+        if (playEnd == "minPlayEnd") {
+            return duration >= this.minPlayEnd;
+        else
+            return duration >=
     }
 }
