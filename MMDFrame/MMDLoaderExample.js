caef862730f0d43da320067a88a9829db1995b4c
ChenZihan <64928288+ChenZihan-sudo@users.noreply.github.com>
2022-04-09 19:03:50 +0800

    version 2.0

--- MMDFrame/MMDLoaderExample.js
+++ MMDFrame/MMDLoaderExample.js
@@ -26,6 +26,14 @@ const plane = new THREE.Mesh(__planeGeometry, planeMaterial);
 plane.rotateX(Math.PI / 2);
 scene.add(plane);
 
+var PointLight = new THREE.PointLight(0xffffff, 0.1);//点光源
+PointLight.position.set(0, 0, 50);
+PointLight.castShadow = true;
+PointLight.shadow.bias = -0.0015;
+PointLight.shadow.mapSize.width = 2048;
+PointLight.shadow.mapSize.height = 2048;
+scene.add(PointLight, addBoxGeometry(20, 20, 20, 0xffffff));
+
 /* 添加坐标辅助系统 */
 var axes = new THREE.AxisHelper(5000);
 scene.add(axes);
@@ -33,19 +41,19 @@ scene.add(axes);
 
 //! Camera 摄像机部分
 var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-camera.position.set(20, 20, 20); //设置相机位置
-camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
+camera.position.set(0, 0, 50); //设置相机位置
+camera.lookAt(0, 20, 0); //设置相机方向(指向的场景对象)
 
 
 //! Renderer 渲染器部分
-var renderer = new THREE.WebGL1Renderer({
+var renderer = new THREE.WebGLRenderer({
     antialias: true, //抗锯齿
     alpha: true, //???
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000, 1);
+renderer.setClearColor(0xffffff, 1);
 renderer.shadowMap.enabled = true; //开启阴影设置
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
@@ -60,24 +68,43 @@ function fixCameraRatio() {
         lastWidth = window.innerWidth;
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix(); //重新计算投影矩阵
-        console.log("重新计算投影矩阵");
     }
 }
 
 var playerConfiguration = {
     mmdFilesPath: {
-        modelFile: "./models/LuoTianyi/luotianyi_v4_ver2.0.pmx",
-        motionFile: "./models/201904202121169044.vmd",
-        audioFile: "./models/CUT.mp3"
+        modelFile: "./mmdFiles/LuoTianyi/luotianyi_v4_ver2.0.pmx",
+        motionFile: "./mmdFiles/201904202121169044.vmd",
+        audioFile: "./mmdFiles/CUT.mp3",
+        cameraFile: "./mmdFiles/201904202121539841.vmd"
     },
     renderFPS: 100,
     enablePhysicWhenMMDPause: true,
-    enablePauseWhenLeaveCurrentPage: true,
+    enablePauseWhenLeaveCurrentPage: false,
     mmdAnimationHelper: {
-        sync: true,
+        sync: false,
         pmxAnimation: true
-    }
+    },
+    playEnd: "maxPlayEnd",
+    loopPlay: true
 }
 
 let playManager = new MMDPlayManager(playerConfiguration);
-playManager.onLoad(() => { console.log("文件加载完成"); })
+playManager.onLoad(() => { console.log("MMD文件全部加载完成"); })
+
+let feedbackParams = {
+    fileOnLoad: {
+
+    },
+    mmdPlayerLoaded: {
+
+    },
+    whenAnimationRender: {
+        userFeedback: () => {
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            fixCameraRatio();
+        }
+    }
+};
+
+playManager.loadEventHolder.addFeedback(feedbackParams);
